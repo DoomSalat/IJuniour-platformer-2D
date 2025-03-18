81be9c93908fd8dedbdf0aa8997272ae0ac679ab
Salatnicha <k.zaitsev@tushitesvet.ru>
2025-03-18 12:37:31 +0300

    Обновлён IsGrounded. Исключает коллайдеры собственного объекта

--- Assets/Scripts/Creatures/Player/Jumper.cs
+++ Assets/Scripts/Creatures/Player/Jumper.cs
@@ -1,4 +1,5 @@
 using System.Collections;
+using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using UnityEngine;
 
@@ -18,6 +19,7 @@ public class Jumper : MonoBehaviour
 	[FoldoutGroup("Ground")][SerializeField] private LayerMask _groundLayer;
 
 	private Rigidbody2D _rigidbody;
+	private List<Collider2D> _selfColliders = new List<Collider2D>();
 
 	private bool _isJumping = false;
 	private bool _isCanGrounded = true;
@@ -26,6 +28,37 @@ public class Jumper : MonoBehaviour
 	private void Awake()
 	{
 		_rigidbody = GetComponent<Rigidbody2D>();
+		InitializateSelfColliders();
+	}
+
+	private void InitializateSelfColliders()
+	{
+		Collider2D[] childColliders = GetComponentsInChildren<Collider2D>();
+
+		foreach (Collider2D childCollider in childColliders)
+		{
+			if (childCollider != null && (_groundLayer & (1 << childCollider.gameObject.layer)) != 0)
+			{
+				_selfColliders.Add(childCollider);
+			}
+		}
+
+		if (TryGetComponent<Collider2D>(out var selfCollider) && (_groundLayer & (1 << selfCollider.gameObject.layer)) != 0)
+		{
+			_selfColliders.Add(selfCollider);
+		}
+	}
+
+	private bool IsSelfCollider(Collider2D collider)
+	{
+		foreach (Collider2D selfCollider in _selfColliders)
+		{
+			if (collider == selfCollider)
+			{
+				return true;
+			}
+		}
+		return false;
 	}
 
 	public bool CanJump()
@@ -44,12 +77,20 @@ public class Jumper : MonoBehaviour
 
 	public bool IsGrounded()
 	{
-		if (_isJumping || _isCanGrounded == false)
+		if (_isCanGrounded == false)
 			return false;
 
-		Collider2D hit = Physics2D.OverlapCircle(_groundCheckPoint.position, _groundCheckRadius, _groundLayer);
+		Collider2D[] hits = Physics2D.OverlapCircleAll(_groundCheckPoint.position, _groundCheckRadius, _groundLayer);
 
-		return hit != null;
+		for (int i = 0; i < hits.Length; i++)
+		{
+			if (hits[i] != null && IsSelfCollider(hits[i]) == false)
+			{
+				return true;
+			}
+		}
+
+		return false;
 	}
 
 	public void FixedForce(bool isJumped)
